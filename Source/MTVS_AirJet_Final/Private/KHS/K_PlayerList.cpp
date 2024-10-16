bb21cf6c832ab21f0bd1c3ae04c5cc5477282026
Christopher-Emunah-Kim <hashin.chris@gmail.com>
2024-10-16 17:24:28 +0900

    Server Network작업
    1. Json 슈팅방식 적용
    2. WebBrowser 플러그인 추가
    3. SessionUI작업 다수

---
+++ Source/MTVS_AirJet_Final/Private/KHS/K_PlayerList.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KHS/K_PlayerList.h"
+
+void UK_PlayerList::Setup(UK_ServerWidget* InParent , uint32 InIndex)
+{
+	Parent = InParent;
+	Index = InIndex;
+}
+
+bool UK_PlayerList::bIsCommander()
+{
+	return false;
+}
---
+++ Source/MTVS_AirJet_Final/Public/KHS/K_PlayerList.h
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "K_PlayerList.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MTVS_AIRJET_FINAL_API UK_PlayerList : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	
+	// 위젯 바인딩 ================================================
+
+	UPROPERTY(EditDefaultsOnly, meta = (BindWidget))
+	class UTextBlock* PlayerNum; // PlayerNum
+
+	UPROPERTY(EditDefaultsOnly, meta = (BindWidget))
+	class UTextBlock* PlayerNicName; // PlayerNicName
+
+	UPROPERTY(EditDefaultsOnly, meta = (BindWidget))
+	class UTextBlock* PlayerType; // PlayerType
+
+	UPROPERTY(EditDefaultsOnly, meta = (BindWidget))
+	class UCheckBox* IsCommander; // 지휘관여부
+	
+	// Variables    ================================================
+
+	UPROPERTY()
+	class UK_ServerWidget* Parent;
+
+	uint32 Index;
+
+	
+	UPROPERTY(BlueprintReadOnly)
+	bool Selected = false; // 서버를 선택했는지, 아닌지 선언(Event Graph에서 사용)
+
+	// Functions    ================================================
+	void Setup(class UK_ServerWidget* InParent, uint32 InIndex);
+
+	UFUNCTION()
+	bool bIsCommander();
+
+
+};
