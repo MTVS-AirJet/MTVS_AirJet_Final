22eaf8d3a636257887a0e0e834f359548dfb7213
Christopher-Emunah-Kim <hashin.chris@gmail.com>
2024-10-28 16:44:55 +0900

    Teleportbox

--- Source/MTVS_AirJet_Final/Private/KHS/K_CesiumTeleportBox.cpp
+++ Source/MTVS_AirJet_Final/Private/KHS/K_CesiumTeleportBox.cpp
@@ -33,11 +33,15 @@ void AK_CesiumTeleportBox::BeginPlay()
 {
 	Super::BeginPlay();
 
-    DestLongitude = 34.0194 ;
-
-    DestLatitude = -118.411;
+    //(임시) LA위경도
+	DestLongitude = 34.0194 ;
+	DestLatitude = -118.411;
 	
 	//BoxComp->OnComponentBeginOverlap.AddDynamic(this , &AK_CesiumTeleportBox::OnMyBoxBeginOverlap);
+
+    /*DestLatitude = KGameInstance->MissionData.latitude;
+    DestLongitude = KGameInstance->MissionData.longitude;*/
+
 } 
 
 // Called every frame
